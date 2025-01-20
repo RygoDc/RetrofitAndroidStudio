09294ccfb4337b0d3a27217968ffee339cb8c102
Rodrigo <rdc.1985@gmail.com>
2025-01-20 10:26:06 +0100

    coctail

---
+++ app/src/main/java/com/rygodc/retrofitexample/Drinks.java
@@ -0,0 +1,20 @@
+package com.rygodc.retrofitexample;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drinks {
+    @SerializedName("drinks")
+    List<Coctail> drinks = new ArrayList<>();
+
+    public class Coctail{
+        @SerializedName("strDrink")
+        public String coctailName;
+        @SerializedName("strDrinkThumb")
+        public String coctailImageUrl;
+        @SerializedName("idDrink")
+        public String coctailIid;
+    }
+}
